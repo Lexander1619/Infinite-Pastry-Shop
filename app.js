85bb5152a9f3fe03f5cab32750736d7b63d2cf90
Lexander1619 <93643342+Lexander1619@users.noreply.github.com>
2021-11-03 16:06:20 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// show cart
+
+(function(){
+const cartInfo = document.getElementById('car-info');
+const cart = document.getElementById('cart');
+
+ cartInfo.addEventListener('click',function(){
+  cart.classList.toggle('show-cart');
+ });
+})();
+
+//add items to the cart
+
+(function(){
+
+})();
